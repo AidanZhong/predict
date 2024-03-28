6853a669a9c024ef2b9ef3a85eec58c5b5d229b3
50601 <506017129@qq.com>
2024-03-28 22:37:41 +0800

    calculate gradient

--- GradientDescent.py
+++ GradientDescent.py
@@ -1,9 +1,23 @@
-import params
-from params import param_count
+from typing import List
+
+from hypothesis import hypothesis
 
 
-def cal_gradient(index:int)->float:
+def cal_gradient(index: int, x: List[float], y: List[float], z: List[float]) -> float:
     res = 0
-    for i in range(param_count):
-        res += hypothesis
-
+    nn = min(x.__len__(), y.__len__(), z.__len__())
+    for i in range(nn):
+        item = hypothesis(x[i], y[i]) - z[i]
+        if index == 0:
+            item *= 1
+        elif index == 1:
+            item *= x[i]
+        elif index == 2:
+            item *= (x[i] ** 2)
+        elif index == 3:
+            item *= y[i]
+        elif index == 4:
+            item *= (y[i] ** 2)
+        res += item
+    res /= nn
+    return res
