e34417b0815663f4ad7209e767d9fbf3da7ed9ba
qinkunzhong <xiaofang1058@gmail.com>
2024-03-30 23:32:55 +0800

    add output

--- predict.py
+++ predict.py
@@ -4,3 +4,9 @@ from utils.CsvReader import *
 print('start')
 read_csv_to_fill_data()
 train()
+print('output is h(x,y)=' + '{:.4f}'.format(THETA[0]) +
+      '+' + '{:.4f}'.format(THETA[1]) + 'x' +
+      '+' + '{:.4f}'.format(THETA[2]) + 'x^2' +
+      '+' + '{:.4f}'.format(THETA[3]) + 'y' +
+      '+' + '{:.4f}'.format(THETA[4]) + 'y^2'
+      )
